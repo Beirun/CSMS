e16cde96be570bcf4b0a7d53d1cc52c679c5c5b0
unknown <TT637@ccs.local>
2025-02-14 11:19:07 +0800

    changes

---
+++ client/ccs-sitin-monitoring-system/src/components/EditProfileForm.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-[80%] h-[80%] flex justify-center items-center">
+        </div>
+</template>
---
+++ client/ccs-sitin-monitoring-system/src/views/ProfileView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="w-screen h-screen flex justify-center items-center">
+        <h1>Profile</h1>
+    </div>
+</template>
